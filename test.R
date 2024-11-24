92d28977d2e5beb9d5b29776aa1f3fc1d779ca1f
songzhewu <138423356+songzhewu@users.noreply.github.com>
2024-11-24 17:51:44 +0800

    Create test.R

---
+++ test.R
@@ -0,0 +1,21 @@
+# 載入必要套件
+library(quantmod)
+library(TTR)
+
+base_dir = getwd()
+
+
+# 設定工作目錄或提供絕對路徑
+setwd(base_dir) 
+
+# 載入外部函式: security_data.R計算均線, KD, RSI數值, 布林格通道
+source("stock_data_fetch.R")
+
+
+## 輸入股碼與時間區間
+symbol<- '2330.TW'
+end_date <- Sys.Date()
+start_date <- end_date-365
+data <- stock_data_fetch(symbol, start_date, end_date)
+
+head(data)
